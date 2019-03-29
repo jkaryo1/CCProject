25f19857971b6c45df0fd5b22b5d7aa123522d2f
LionelEisenberg <lionel.eisenberg@gmail.com>
2019-03-29 14:28:52 -0400

    added python  script that creates a unsorted and a sorted array of specified length and write them to a file

---
+++ create_random.py
@@ -0,0 +1,16 @@
+import os
+import random
+import sys
+
+length = int(sys.argv[1])
+array = []
+for i in range(length):
+    rand = random.randint(-999999, 999999)
+    array.append(rand)
+
+sorted_array = sorted(array)
+f = open('input_' + str(length), 'w')
+f2 = open('sorted_' + str(length), 'w')
+for i in range(len(array)):
+    f.write(str(array[i]) + "\n")
+    f2.write(str(sorted_array[i]) + "\n")
