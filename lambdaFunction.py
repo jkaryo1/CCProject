aecf54c06c1bfcfddfcbebaefe83a5c707110526
Lionel Eisenberg <lioneleisenberg@Lionels-MacBook-Pro.local>
2019-04-19 16:31:37 -0400

    function unzips file

--- lambdaFunction.py
+++ lambdaFunction.py
@@ -4,6 +4,7 @@ import sys
 import uuid
 import json
 import subprocess
+from zipfile import ZipFile
 
 s3_client = boto3.client('s3')
 
@@ -28,30 +29,34 @@ def handler(event, context):
     for record in event['Records']:
         # This gets the name of the file that was uploaded
         key = record['s3']['object']['key']
-        if "main_file.py" in key:
-            # This line gets the bucket name from the event that triggered it.
-            source_bucket = record['s3']['bucket']['name']
-            target_bucket = 'jhu-cloud-target2'
-            # Creates a local temp path for the file we download.
-            download_path = '/tmp/{}'.format(key)
-            # crerate path the input for the test will be on
-            testing_path = '/tmp/test-file'
-            testing_key = 'input_10000'
-            # Creates a local temp path for the results.
-            upload_path = '/tmp/result-{}'.format(key)
+        # This line gets the bucket name from the event that triggered it.
+        source_bucket = record['s3']['bucket']['name']
+        target_bucket = 'jhu-cloud-target2'
+        # Creates a local temp path for the file we download.
+        zip_path = '/tmp/{}'.format(key)
+        zip_dir = '/tmp/'
+        main_path = '/tmp/main_file.py'
+        # crerate path the input for the test will be on
+        testing_path = '/tmp/test-file'
+        testing_key = 'input_10000'
+        # Creates a local temp path for the results.
+        upload_path = '/tmp/result-{}'.format(key)
 
-            # get files from bucket
-            s3_client.download_file(source_bucket, key, download_path)
-            s3_client.download_file(source_bucket, testing_key, testing_path)
+        # get files from bucket
+        s3_client.download_file(source_bucket, key, zip_path)
+        s3_client.download_file(source_bucket, testing_key, testing_path)
 
-            success = test_file(download_path, testing_path)
-            print(success)
-            file = open(upload_path, 'w')
-            file.write(str(success) + "\n")
-            file.close()
+        # From zip get main_file
+        with ZipFile("file.zip","r") as zip_ref:
+            zip_ref.extractall(main_target_dir)
+            print('works')
 
-            # upload file to target bucket.
-            s3_client.upload_file(upload_path, target_bucket, key)
-            return
-        else:
-            print(key)
+        success = test_file(main_path, testing_path)
+        print(success)
+        file = open(upload_path, 'w')
+        file.write(str(success) + "\n")
+        file.close()
+
+        # upload file to target bucket.
+        s3_client.upload_file(upload_path, target_bucket, key)
+        return
