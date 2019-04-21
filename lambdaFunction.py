3c2416b494a99b95fb2b85ba51717f96aed12ff6
Lionel Eisenberg <lioneleisenberg@Lionels-MacBook-Pro.local>
2019-04-21 19:54:39 -0400

    just added unzipping fuunctionoality to lambda

--- lambdaFunction.py
+++ lambdaFunction.py
@@ -20,43 +20,53 @@ def test_file(download_path, testing_path):
     except EnvironmentError:
         print("error")
 
-    sorting_func = "bubble"
-    result = subprocess.run(['python3.7', FILEPATH, sorting_func, TESTPATH], stdout=subprocess.PIPE)
-    array = list(map(int, result.stdout.decode('utf-8').splitlines()))
-    return array == arraySorted
+    sorting_funcs = ['bubble', 'selection', 'insertion']
+    results = {}
+    for sorting_func in sorting_funcs:
+        result = subprocess.run(['python3.7', FILEPATH, sorting_func, TESTPATH], stdout=subprocess.PIPE)
+        array = list(map(int, result.stdout.decode('utf-8').splitlines()))
+        results[sorting_func] = array == arraySorted
+    return results
 
 def handler(event, context):
     for record in event['Records']:
+
         # This gets the name of the file that was uploaded
         key = record['s3']['object']['key']
-        # This line gets the bucket name from the event that triggered it.
-        source_bucket = record['s3']['bucket']['name']
-        target_bucket = 'jhu-cloud-target2'
-        # Creates a local temp path for the file we download.
-        zip_path = '/tmp/{}'.format(key)
-        zip_dir = '/tmp/'
-        main_path = '/tmp/main_file.py'
-        # crerate path the input for the test will be on
-        testing_path = '/tmp/test-file'
-        testing_key = 'input_10000'
-        # Creates a local temp path for the results.
-        upload_path = '/tmp/result-{}'.format(key)
+        if ".zip" in key:
+            # Get the version number of the commit.
+            version = key[:1]
+            print(version)
+            target_key = str(version) + '.json'
+            # This line gets the bucket name from the event that triggered it.
+            source_bucket = record['s3']['bucket']['name']
+            target_bucket = 'jhu-cloud-target2'
+            # Creates a local temp path for the file we download.
+            zip_path = '/tmp/{}'.format(key)
+            zip_dir = '/tmp/'
+            main_path = '/tmp/CloudComp-Testing/main_file.py'
+            # crerate path the input for the test will be on
+            testing_path = '/tmp/test-file'
+            testing_key = 'input_1000'
+            # Creates a local temp path for the results.
+            upload_path = '/tmp/result-{}.json'.format(key)
 
-        # get files from bucket
-        s3_client.download_file(source_bucket, key, zip_path)
-        s3_client.download_file(source_bucket, testing_key, testing_path)
+            # get files from bucket
+            s3_client.download_file(source_bucket, key, zip_path)
+            s3_client.download_file(source_bucket, testing_key, testing_path)
 
-        # From zip get main_file
-        with ZipFile("file.zip","r") as zip_ref:
-            zip_ref.extractall(main_target_dir)
-            print('works')
+            # From zip get main_file
+            with ZipFile(zip_path,"r") as zip_ref:
+                zip_ref.extractall(zip_dir)
 
-        success = test_file(main_path, testing_path)
-        print(success)
-        file = open(upload_path, 'w')
-        file.write(str(success) + "\n")
-        file.close()
+            # write results from the testing to a json
+            results = test_file(main_path, testing_path)
+            results_json = json.dumps(results)
+            print(results_json)
+            file = open(upload_path, 'w')
+            file.write(results_json)
+            file.close()
 
-        # upload file to target bucket.
-        s3_client.upload_file(upload_path, target_bucket, key)
+            # upload file to target bucket.
+            s3_client.upload_file(upload_path, target_bucket, target_key)
         return
