8180950219eda84cc9c353a97ba32093f1ce6b9d
Lionel Eisenberg <lioneleisenberg@Lionels-MacBook-Pro.local>
2019-04-29 22:57:50 -0400

    added port selection to main file

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -135,4 +135,6 @@ def getJson(bucket_name, num_commits, s3):
     return result
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=5002)
+    app.debug = True
+    port = int(os.environ.get("PORT", 5002))
+    app.run(host='0.0.0.0', port=port)
