1531b9873a2451531dd003e06e2fb5a0c2dde298
Jack Karyo <jkaryo1@jhu.edu>
2019-05-10 13:09:03 -0400

    boom goes the dynamite

--- git_bisect.py
+++ git_bisect.py
@@ -7,7 +7,7 @@ length = sys.argv[1]
 # Times git bisect run of testing suite
 # MUST BE CALLED FROM INSIDE TESTING REPO
 start = time.time()
-proc = subprocess.Popen(("git bisect start 658e93d 9ef8eb6").split())
+proc = subprocess.Popen(("git bisect start 470e956 0f15c53").split())
 proc.wait()
 proc = subprocess.Popen(("git bisect run python ../CCProject/test_sorting.py " + length).split())
 proc.wait()
